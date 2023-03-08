67003d95d640e5dfd00bf75faf483671ecd4772f
Leander Lismond <36822732+l5r@users.noreply.github.com>
2023-03-08 18:13:32 +0100

    Allow adding products to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_order_form, only: %i[ new ]
 
   # GET /orders
   def index
@@ -12,7 +13,10 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
-    @order = Order.new(order_form_id: params[:order_form_id])
+    @order = Order.new(order_form: @order_form)
+    @order_form.products.each do |product|
+      @order.order_lines.build(product: product)
+    end
   end
 
   # GET /orders/1/edit
@@ -26,7 +30,7 @@ class OrdersController < ApplicationController
     @order.order_form_id = params[:order_form_id]
 
     if @order.save
-      redirect_to @order, notice: "Order was successfully created."
+      redirect_to @order
     else
       render :new, status: :unprocessable_entity
     end
@@ -48,8 +52,14 @@ class OrdersController < ApplicationController
       @order = Order.includes(:order_form, order_lines: :product).find(params[:id])
     end
 
+    def set_order_form
+      @order_form = OrderForm.find(params[:order_form_id])
+    end
+
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:order_form_id, :name, :email, :phone, :comment)
+      params.require(:order)
+        .permit(:order_form_id, :name, :email, :phone, :comment,
+               order_lines_attributes: [:id, :product_id, :amount])
     end
 end
